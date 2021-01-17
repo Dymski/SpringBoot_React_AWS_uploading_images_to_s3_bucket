8223ce2f9bb62dced51929a3e7d31a3593831074
Dymski <w.radymski@hotmail.com>
2021-01-17 10:47:44 +0100

    initial commit

---
+++ src/main/java/com/dymski/aws_react_spring_imageuploader/AwsReactSpringImageuploaderApplication.java
@@ -0,0 +1,13 @@
+package com.dymski.aws_react_spring_imageuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsReactSpringImageuploaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsReactSpringImageuploaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dymski/aws_react_spring_imageuploader/AwsReactSpringImageuploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dymski.aws_react_spring_imageuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsReactSpringImageuploaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
